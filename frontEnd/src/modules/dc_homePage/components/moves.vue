da98de75618c7b42b960ed2ea6c3a9339022fb21
zaoxier <2547546090@qq.com>
2018-03-14 17:14:05 +0800

    test

--- frontEnd/src/modules/dc_homePage/components/moves.vue
+++ frontEnd/src/modules/dc_homePage/components/moves.vue
@@ -1,69 +1,112 @@
 <template>
- <div class="moves">
-   <el-tabs type="border-card">
-      <el-tab-pane>
-        <span slot="label"><i class="el-icon-date"></i>回复我的</span>
-        <div class="moves-box">
-          <dl>
-            <dt>
-              <span class=""></span>
-            </dt>
-          </dl>
+  <div class="moves">
+    <el-tabs type="border-card">
+      <el-tab-pane :label="'全部动态'">
+        <div class="dynamic" v-for="(item, index) in movesList" :key="index">
+          <div class="head-square">
+            <a><img src="../../../assets/images/bg1.jpg"></a>
+          </div>
+          <div class="dynamic-message">
+            <p class="dynamic-message_title">
+              <span>
+                <a href="#">心境如水:</a>
+              </span>
+              <span>发表了一个帖子
+                <a href="#">{{item.title}}</a>
+              </span>
+              <span>时间:{{item.datetime}}</span>
+            </p>
+            <p class="dynamic-message_content">{{item.detail}}</p>
+            <p class="dynamic-message_pic"><img :src="item.pic" style="width: 100%; height: 100%;"></p>
+          </div>
         </div>
       </el-tab-pane>
-  <el-tab-pane :label="'我的发帖'">我的发帖</el-tab-pane>
-  <el-tab-pane :label="'我的回帖'">我的回帖</el-tab-pane>
-  <el-tab-pane :label="'我收藏的帖子'+'(12)'">我收藏的帖子</el-tab-pane>
-</el-tabs>
- </div>
+      <el-tab-pane :label="'关于我的'"></el-tab-pane>
+    </el-tabs>
+  </div>
 </template>
 
 <script type="text/ecmascript-6">
-export default {
-  data() {
-    return {
-      tableData: [
-        {
-          theme: '2018年春季来了应该穿些什么呢？',
-          plate: '季节',
-          resCheNum: '32/83',
-          lastRes: '昵称+时间'
-        },
-        {
-          theme: '2018年春季来了应该穿些什么呢？',
-          plate: '季节',
-          resCheNum: '32/83',
-          lastRes: '昵称+时间'
-        },
-        {
-          theme: '2018年春季来了应该穿些什么呢？',
-          plate: '季节',
-          resCheNum: '32/83',
-          lastRes: '昵称+时间'
-        },
-        {
-          theme: '2018年春季来了应该穿些什么呢？',
-          plate: '季节',
-          resCheNum: '32/83',
-          lastRes: '昵称+时间'
-        }
-      ]
+  export default {
+    name: "forum",
+    data() {
+      return {
+        movesList: [
+          {
+            title: "一组昨天拍的西式婚礼",
+            datetime: "3月7日",
+            detail:
+              "这对新人之前找我拍的婚纱，昨天能再次为他们拍婚礼我感到非常荣幸。婚礼的规模很小，也就到场了三十来位宾客，但这并不影响欢快喜庆的氛围，再加上天气很好（虽然大晴天拍户外对摄影师来说是个挑战），因此照片看起来充满幸福感。拍摄户外仪式我通常都是双机，一台挂24-70，一台挂70-200，以确保从大场景到特写都能拍到。结束之后会换上超广拍大合照。进到室内吃饭后要看现",
+            pic: require("../../../assets/images/dress1.png")
+          },
+          {
+            title: "一组昨天拍的西式婚礼",
+            datetime: "3月7日",
+            detail:
+              "这对新人之前找我拍的婚纱，昨天能再次为他们拍婚礼我感到非常荣幸。婚礼的规模很小，也就到场了三十来位宾客，但这并不影响欢快喜庆的氛围，再加上天气很好（虽然大晴天拍户外对摄影师来说是个挑战），因此照片看起来充满幸福感。拍摄户外仪式我通常都是双机，一台挂24-70，一台挂70-200，以确保从大场景到特写都能拍到。结束之后会换上超广拍大合照。进到室内吃饭后要看现",
+            pic: require("../../../assets/images/dress1.png")
+          },
+          {
+            title: "一组昨天拍的西式婚礼",
+            datetime: "3月7日",
+            detail:
+              "这对新人之前找我拍的婚纱，昨天能再次为他们拍婚礼我感到非常荣幸。婚礼的规模很小，也就到场了三十来位宾客，但这并不影响欢快喜庆的氛围，再加上天气很好（虽然大晴天拍户外对摄影师来说是个挑战），因此照片看起来充满幸福感。拍摄户外仪式我通常都是双机，一台挂24-70，一台挂70-200，以确保从大场景到特写都能拍到。结束之后会换上超广拍大合照。进到室内吃饭后要看现",
+            pic: require("../../../assets/images/dress1.png")
+          }
+        ]
+      };
     }
-  },
-  components: {},
-  methods: {
-    // this.axios.getApi(res).then(res => {})
-  }
-}
+  };
 </script>
 
 <style lang="less">
-  .moves{
-    padding-left: 10px;
-    width: 940px;
-    .el-tabs__item{
-      &:hover{
-        color: #20a0ff;
+  .moves {
+    .el-tabs__content {
+      padding: 0;
+    }
+    .dynamic {
+      padding: 20px;
+      display: flex;
+      flex-wrap: wrap;
+      border-bottom: 1px #e9e9e9 dashed;
+      a {
+        color: #39a6b1;
+      }
+      .head-square {
+        display: inline-block;
+        width: 52px;
+        height: 52px;
+        img {
+          width: 100%;
+          height: 100%;
+        }
+      }
+      .dynamic-message {
+        font-size: 14px;
+        line-height: 24px;
+        margin-left: 15px;
+        width: 800px;
+        p {
+          margin-bottom: 15px;
+        }
+        &_title {
+          span:last-child {
+            float: right;
+          }
+        }
+        &_content {
+          height: 72px;
+          overflow: hidden;
+          text-overflow: ellipsis;
+          display: -webkit-box;
+          // -webkit-box-orient: vertical;
+          -webkit-line-clamp: 3;
+        }
+        &_pic {
+          height: 400px;
+          width: 400px;
+          border: 1px solid rgb(240, 236, 236);
+        }
       }
     }
   }
--- frontEnd/src/modules/dc_homePage/pages/homePage.vue
+++ frontEnd/src/modules/dc_homePage/pages/homePage.vue
@@ -1,148 +1,164 @@
 <template>
   <div class="homePage">
-    <div class="tab-title">  
-    <el-tabs v-model="activeName" >
-      <el-tab-pane  v-for="(item,index) in tabList" :key="index" :label="item.hp_thirTitle" :name="item.index" >
-        <el-breadcrumb separator-class="el-icon-arrow-right">
-          <el-breadcrumb-item :to="{ path: '/' }">{{homeTitle.websiteName}}</el-breadcrumb-item>
-          <el-breadcrumb-item>{{homeTitle.personalCenter}}</el-breadcrumb-item>
-          <el-breadcrumb-item>{{item.hp_thirTitle}}</el-breadcrumb-item>
-          <el-breadcrumb-item>{{item.hp_fourTitle}}</el-breadcrumb-item>
-        </el-breadcrumb>
-        <div class="content" v-if="index==0">
-          <div class="headinfo">
-            <div class="head-box">
-              <img src="../../../assets/images/bg1.jpg">
-            </div>
-            <p class="nickname">zaozao</p>
-            <div class="grade">
-              <span><img src="">铜级</span>
-              <span>积分：</span>
-            </div>
-            <p><el-button type="primary" icon="el-icon-edit"></el-button></p>
-          </div>
-          <div class="left">
-               <div class="fanFocus">
-              <div class="fanFocus--child">
-                    <a href="#"><span>关注</span></a>
-                    <a href="#"><span>2</span></a>
+    <div class="tab-title">
+      <el-tabs v-model="activeName">
+        <el-tab-pane v-for="(item,index) in tabList" :key="index" :label="item.hp_thirTitle"
+          :name="item.index">
+          <el-breadcrumb separator-class="el-icon-arrow-right">
+            <el-breadcrumb-item :to="{ path: '/' }">{{homeTitle.websiteName}}</el-breadcrumb-item>
+            <el-breadcrumb-item>{{homeTitle.personalCenter}}</el-breadcrumb-item>
+            <el-breadcrumb-item>{{item.hp_thirTitle}}</el-breadcrumb-item>
+            <el-breadcrumb-item>{{item.hp_fourTitle}}</el-breadcrumb-item>
+          </el-breadcrumb>
+          <div class="content" v-if="index==0">
+            <div class="headinfo">
+              <div class="head-box">
+                <a><img src="../../../assets/images/bg1.jpg"></a>
               </div>
+              <p class="nickname">zaozao</p>
+              <div class="grade">
+                <span><img src="">铜级</span>
+                <span>积分：</span>
+              </div>
+              <p>
+                <el-button type="primary" icon="el-icon-edit">编辑</el-button>
+              </p>
+            </div>
+            <div class="left">
+              <div class="fanFocus">
                 <div class="fanFocus--child">
-                <a href="#"><span>粉丝</span></a>
-                  <a href="#"><span>0</span></a>
-                </a>
+                  <a href="#">
+                    <span>关注</span>
+                  </a>
+                  <a href="#">
+                    <span>2</span>
+                  </a>
+                </div>
+                <div class="fanFocus--child">
+                  <a href="#">
+                    <span>粉丝</span>
+                  </a>
+                  <a href="#">
+                    <span>0</span>
+                  </a>
+                </div>
               </div>
-              </div>  
-            <el-tabs :tab-position="tabPosition" type="card" @tabs-click="handleClick">
-              <el-tab-pane label="他的动态"></el-tab-pane>
-              <el-tab-pane label="他的相册"><div class="main"></div></el-tab-pane>
-              <el-tab-pane label="他的论坛"></el-tab-pane>
-              <el-tab-pane label="他的好友"><friend-fan></friend-fan></el-tab-pane>
-              <el-tab-pane label="他的资料"></el-tab-pane>
-            </el-tabs>
+              <el-tabs :tab-position="tabPosition" type="card" @tab-click="handleClick">
+                <el-tab-pane label="他的动态"></el-tab-pane>
+                <el-tab-pane label="他的相册">
+                  <div class="main"></div>
+                </el-tab-pane>
+                <el-tab-pane label="他的论坛"></el-tab-pane>
+                <el-tab-pane label="他的好友">
+                  <friend-fan></friend-fan>
+                </el-tab-pane>
+                <el-tab-pane label="他的资料"></el-tab-pane>
+              </el-tabs>
+            </div>
+            <div class="main">
+              <moves v-if="tabChance == 0"></moves>
+              <friend-fan v-else-if="tabChance == 3"></friend-fan>
+            </div>
           </div>
-          <!-- <div class="main">
-            <moves></moves>
-            <friend-fan></friend-fan>
-          </div> -->
-        </div>
-      </el-tab-pane>
+        </el-tab-pane>
       </el-tabs>
     </div>
   </div>
 </template>
 
 <script>
-import moves from '../components/moves.vue'
-import friendFan from '../components/friendFan.vue'
+  import moves from "../components/moves.vue";
+  import friendFan from "../components/friendFan.vue";
 
-export default {
-  name: 'homePage_index',
-  data() {
-    return {
-      activeName: 'first',
-      homeTitle: {
-        websiteName: '衣着搭配',
-        personalCenter: '个人中心'
-      },
-      tabList: [
-        {
-          index: 'first',
-          hp_thirTitle: '我的主页',
-          hp_fourTitle: '我的动态'
+  export default {
+    name: "homePage_index",
+    data() {
+      return {
+        activeName: "first",
+        tabChance: 0,
+        homeTitle: {
+          websiteName: "衣着搭配",
+          personalCenter: "个人中心"
         },
-        {
-          index: 'second',
-          hp_thirTitle: '设置',
-          hp_fourTitle: '基本信息'
-        }
-      ],
-      tabPosition: "left"
-      // moves: false
+        tabList: [
+          {
+            index: "first",
+            hp_thirTitle: "我的主页",
+            hp_fourTitle: "我的动态"
+          },
+          {
+            index: "second",
+            hp_thirTitle: "设置",
+            hp_fourTitle: "基本信息"
+          }
+        ],
+        tabPosition: "left"
+        // moves: false
+      };
+    },
+    components: {
+      moves,
+      friendFan
+    },
+    methods: {
+      handleClick(tab, event) {
+        this.tabChance = tab.index;
+      }
     }
-  },
-  components: {
-    moves,
-    friendFan
-  },
-  methods: {
-    handleClick: function(tab, event) {
-      console.log(event + event.target.getAttribute('id'))
-    }
-  }
-}
+  };
 </script>
 
 <style lang="less">
-.homePage {
-  .tab-title {
+  .homePage {
+    .tab-title {
       margin-left: 100px;
-  }
-  .content{
-    margin: 0 100px;
-    width: 1200px;
-      .headinfo{
+    }
+    .content {
+      margin: 0 100px;
+      width: 1200px;
+      .headinfo {
         position: relative;
         margin-top: 100px;
         padding-top: 75px;
         height: 100px;
         border: 1px solid #e0e0e0;
-        .head-box{
+        .head-box {
           width: 150px;
           height: 150px;
           margin-left: -75px;
           position: absolute;
           left: 50%;
           top: -75px;
-          img{
+          img {
             width: 100%;
             height: 100%;
             border-radius: 50%;
           }
         }
-        p{
+        p {
           text-align: center;
         }
-        .nickname{
+        .nickname {
           font-size: 18px;
           font-weight: bold;
         }
-        .grade{
+        .grade {
           height: 20px;
           margin: 5px 0 10px;
           text-align: center;
         }
-        .el-icon-edit:before {
-          // content: "\E61C";
-          background-color: #fff;
-              content: "\E611";
+        .el-button {
+          padding: 8px 10px;
+          .el-icon-edit:before {
+            content: "\E61C";
+          }
         }
-    }
-    .left{
-      float: left;
-      margin-top: 20px;
-      width: 240px;
-        .fanFocus{
+      }
+      .left {
+        float: left;
+        margin-top: 20px;
+        width: 240px;
+        .fanFocus {
           width: 100%;
           padding-top: 10px;
           border-top: 2px #999 solid;
@@ -151,64 +167,63 @@ export default {
           border-bottom: 1px solid #e4e7ed;
           display: flex;
           position: relative;
-          .fanFocus--child{
-            // 
+          .fanFocus--child {
+            //
             flex: 1;
             text-align: center;
             font-size: 16px;
-            &:first-child{
-              &:after{
-                   content: '';
-    width: 1px;
-    height: 50px;
-    position: absolute;
-    left: 50%;
-    top: 10px;
-    background: #e0e0e0;
+            &:first-child {
+              &:after {
+                content: "";
+                width: 1px;
+                height: 50px;
+                position: absolute;
+                left: 50%;
+                top: 10px;
+                background: #e0e0e0;
               }
-            }            
-            span{
+            }
+            span {
               display: block;
               margin-bottom: 5px;
               height: 25px;
             }
-            a{
+            a {
               color: #5f686b;
-               &:hover{
-              text-decoration: underline;
-              color: #409EFF;
-            }
+              &:hover {
+                text-decoration: underline;
+                color: #409eff;
+              }
             }
           }
         }
-        .el-tabs{
-
-               margin-top: 10px;
-            .el-tabs__item.is-left{
-                 width: 240px;
-       text-align: center;
-       &:first-child{
-          // border-right: 2px solid #e4e7ed;
-          border-top: 2px #999 solid;
-      }
-     }
+        .el-tabs {
+          margin-top: 10px;
+          .el-tabs__item.is-left {
+            width: 240px;
+            text-align: center;
+            &:first-child {
+              // border-right: 2px solid #e4e7ed;
+              border-top: 2px #999 solid;
+            }
+          }
         }
-   
-      .is-active{
-        border-right-color: #e4e7ed;
+
+        .is-active {
+          border-right-color: #e4e7ed;
+        }
       }
-    }
-      .main{
+      .main {
         width: 940px;
         padding-left: 10px;
         float: left;
-            margin-top: 20px;
-        .el-tabs__item{
+        margin-top: 20px;
+        .el-tabs__item {
           font-size: 16px;
         }
       }
+    }
   }
-}
 </style>
 
 
